15988657ed5fef5f00c5dea33c18049b4bb4e3e9
Anne Chung <annechung301@gmail.com>
2021-09-29 05:51:16 -0400

    Add experience and project sections

---
+++ src/components/Projects.js
@@ -0,0 +1,21 @@
+import Grid from '@mui/material/Grid'
+import Container from '@mui/material/Container'
+
+function Projects(props) {
+  return (
+    <div id="projects" className="section text">
+      <Container>
+      <h2 className="section-header"><span className="underline">Projects</span></h2>
+      community fridge 
+      - MMR
+      - parrot
+      - chess
+      - safe.net
+      - Waterloo Rush
+      - moby sick
+      </Container>
+    </div>
+  )
+}
+
+export default Projects
