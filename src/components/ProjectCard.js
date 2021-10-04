42ebd07de824c533c6f3eab880433eaaac9086cf
Anne Chung <annechung301@gmail.com>
2021-10-03 22:01:19 -0400

    Update project card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,7 +1,5 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Collapse from '@mui/material/Collapse';
 import Typography from '@mui/material/Typography';
 import Skillset from './Skillset';
 import Link from '@mui/material/Link';
@@ -19,20 +17,18 @@ function ProjectCard(props) {
   return (
     <Card onClick={handleExpandClick} className="project-card">
       <CardContent className="project-content">
+        {props.githubLink ? 
+          <Link underline="none" target="_blank" href={props.githubLink} className="github">
+            &nbsp;<GitHub/>
+          </Link>
+          : 
+          <></>}
         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
           {props.header}
         </Typography>
-        <div className="project-card-title">
-          <Typography variant="h5" component="div">
-            {props.title}
-          </Typography>
-          {props.githubLink ? 
-            <Link underline="none" target="_blank" href={props.githubLink}>
-              &nbsp;<GitHub/>
-            </Link>
-            : 
-            <></>}
-        </div>
+        <Typography variant="h5" component="div">
+          {props.title}
+        </Typography>
         <Typography variant="body2">
           <br></br>
           {props.description}
